fadcf07af4ba03802a49116d40496681b4924e9d
DraksBG <52380770+DraksBG@users.noreply.github.com>
2022-05-01 21:50:47 +0300

    Initial commit

---
+++ e-shop-app/src/App.js
@@ -0,0 +1,12 @@
+import Header from "./Components/Header/index"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Header />
+    </div>
+  );
+}
+
+export default App;
